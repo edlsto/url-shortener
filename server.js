bbeb50d377e8432da29b7d62cf71355082852347
Edward Stoner <edwardlstoner@gmail.com>
2020-05-17 13:33:42 -0600

    Add Mongo, add post route

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+require("dotenv").config();
+var bodyParser = require("body-parser");
 
-var cors = require('cors');
+var cors = require("cors");
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
+/** this project needs a db !! **/
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-/** this project needs a db !! **/ 
+var Schema = mongoose.Schema;
+
+var urlSchema = new Schema({
+  url: String, // String is shorthand for {type: String}
+  shorturl: Number,
+});
+
+var Url = mongoose.model("Url", urlSchema);
+
 // mongoose.connect(process.env.DB_URI);
 
 app.use(cors());
@@ -19,19 +34,28 @@ app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/shorturl/:id", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
+app.post("/api/shorturl/new", (req, res) => {
+  let url = req.body.url;
+  let newRecord = new Url({ url: url, shorturl: Date.now() });
+  newRecord.save(function (error, data) {
+    if (error) return error;
+  });
+  res.json({ url: url });
+});
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
